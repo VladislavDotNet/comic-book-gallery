73afc1b8a4624e8c8190b910614d51224bd19c55
Vladislav <Vladislav@KOMPUTER>
2022-10-31 21:37:23 +0700

    123

--- Controllers/ComicBooksController.cs
+++ Controllers/ComicBooksController.cs
@@ -1,8 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+         public ActionResult Detail ()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
+            {
+                return Redirect("/");
+            }
+            return Content("Hello from the comic books controller!");  
+        }
     }
-}
+}   
+
+
+
