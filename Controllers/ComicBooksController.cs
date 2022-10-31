0cf6b6dcf1cbefef8df58a03536ff2d24dfd3c5f
Vladislav <Vladislav@KOMPUTER>
2022-10-31 21:49:47 +0700

    df

--- Controllers/ComicBooksController.cs
+++ Controllers/ComicBooksController.cs
@@ -7,11 +7,7 @@ namespace ComicBookGallery.Controllers
     {
          public ActionResult Detail ()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Monday)
-            {
-                return Redirect("/");
-            }
-            return Content("Hello from the comic books controller!");  
+            return View();
         }
     }
 }   
