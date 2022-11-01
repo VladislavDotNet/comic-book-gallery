1e82dd4fcee22f5dc9baa513e8b2afb726082ccc
Vladislav <Vladislav@KOMPUTER>
2022-11-01 22:31:28 +0700

    dfjgh

--- Controllers/ComicBooksController.cs
+++ Controllers/ComicBooksController.cs
@@ -7,6 +7,21 @@ namespace ComicBookGallery.Controllers
     {
          public ActionResult Detail ()
         {
+            ViewBag.SeriesTitle = "The Amazing Spider-Man!";
+            ViewBag.issueNumber = 700;
+            ViewBag.description = "<p>Final alieudghiadefhgiuaefgiurefhiguher</p>";
+            ViewBag.artists = new string[]
+            {
+                "Script: Dan Slott",
+                "Pencils: Humberto ramos",
+                "Inks: Victor Olazaba",
+                "Colors: Edgar Delgado",
+                "Letters: Chris Eliopoulos"
+
+            };
+
+
+
             return View();
         }
     }
